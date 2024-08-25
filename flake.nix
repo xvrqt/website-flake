98de0b7888d836572e30121bc2eef701965b80ee
xvrqt <git@xvrqt.com>
2024-08-25 13:43:36 -0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
           sites.homepage.nixosModules.default # xvrqt homepage
           sites.cs4600.nixosModules.default # cs4600 class projects
           sites.moomin-orb.nixosModules.default # View images in Moomin's Orb
-          sites.game-of-life.nixosModules.default
+          sites.game-of-life.nixosModules.aarch64-linux.default
         ];
       };
     };
