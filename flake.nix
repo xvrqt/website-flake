2e2293b0d290f31e0f9c0a802884a7b77a5b2562
xvrqt <git@xvrqt.com>
2025-02-25 10:40:45 -0800

    test

--- flake.nix
+++ flake.nix
@@ -8,9 +8,10 @@
     moomin-orb.url = "github:xvrqt/moomin-orb/deploy";
     game-of-life.url = "github:xvrqt/game-of-life-demo/deploy";
     graphics.url = "github:xvrqt/graphics-website";
+    http.url = "git+https://git.irlqt.net/crow/http-status-codes-website.git";
   };
 
-  outputs = {flake-utils, ...} @ sites:
+  outputs = { flake-utils, ... } @ sites:
     flake-utils.lib.eachDefaultSystem (system: {
       nixosModules = {
         default = {
@@ -24,6 +25,7 @@
             #   - creates an option under: services.websites.sites.<site>
             #   - configures a virtual host for nginx
             #   - creates a package and installs itself
+            sites.http.nixosModules.default
             sites.homepage.nixosModules.default # xvrqt homepage
             sites.dino-game.nixosModules.default # A game with dinosaurs (static site)
             sites.cs4600.nixosModules.default # cs4600 class projects
