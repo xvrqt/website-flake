ab41840506ff4630f4d969311b58ed4ee0bc7635
xvrqt <git@xvrqt.com>
2025-07-16 20:07:45 -0700

    Added in the system stuff again :[

--- flake.nix
+++ flake.nix
@@ -3,6 +3,10 @@
     # Used to store and retrieve encrypted DNS challenge tokens
     secrets.url = "git+https://git.irlqt.net/crow/secrets-flake";
 
+    # Used for building the site
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
     # Websites
     cs4600.url = "git+https://git.irlqt.net/crow/cs4600-website-flake";
     dino-game.url = "git+https://git.irlqt.net/crow/dino-website-flake";
@@ -12,46 +16,53 @@
     moomin-orb.url = "git+https://git.irlqt.net/crow/moomin-orb-website-flake";
   };
 
-  outputs = { flake-utils, secrets, ... } @ sites:
+  outputs = { utils, nixpkgs, secrets, ... } @ sites:
+    let
+      wrapped = utils.lib.eachDefaultSystem
+        (system:
+          {
+            nixosModules = {
+              default = {
+                imports = [
+                  # Allows for use of secrets in the NixOS Module below
+                  secrets.nixosModules.default
+                  # Main option to enable the websites
+                  # Imports submodules: webserver (enables nginx, configures auto fetching certificates)
+                  ./nixosModule.nix
+
+                  # Import individual sites
+                  # Each site
+                  #   - creates an option under: services.websites.sites.<site>
+                  #   - configures a virtual host for nginx
+                  #   - creates a package and installs itself
+                  sites.cs4600.nixosModules.default # cs4600 class projects
+                  sites.dino-game.nixosModules.default # A game with dinosaurs
+                  sites.homepage.nixosModules.default # xvrqt homepage
+                  sites.http.nixosModules.default # A site that generates HTTP status codes
+                  sites.moomin-orb.nixosModules.default # View images in Moomin's Orb
+                  sites.game-of-life.nixosModules.${system}.default
+                ];
+              };
+            };
+          });
+      nixosModules = wrapped.nixosModules;
+    in
     {
-      nixosModules =
-        {
-          default = { pkgs, ... }: {
-            imports = [
-              # Allows for use of secrets in the NixOS Module below
-              secrets.nixosModules.default
-              # Main option to enable the websites
-              # Imports submodules: webserver (enables nginx, configures auto fetching certificates)
-              ./nixosModule.nix
-
-              # Import individual sites
-              # Each site
-              #   - creates an option under: services.websites.sites.<site>
-              #   - configures a virtual host for nginx
-              #   - creates a package and installs itself
-              sites.cs4600.nixosModules.default # cs4600 class projects
-              sites.dino-game.nixosModules.default # A game with dinosaurs (static site)
-              sites.homepage.nixosModules.default # xvrqt homepage
-              sites.http.nixosModules.default # A site that generates HTTP status codes
-              sites.moomin-orb.nixosModules.default # View images in Moomin's Orb
-              sites.game-of-life.nixosModules.${pkgs.system}.default
-            ];
-          };
-        };
-
-      # Convenience function to setup a simple reverse proxy
-      setupReverseProxy = { domain, proxy_socket }: {
-        config.services.nginx.virtualHosts.${domain} = {
-          forceSSL = true;
-          acmeRoot = null;
-          enableACME = true;
-
-          locations."/" = {
-            proxyPass = proxy_socket;
-            proxyWebsockets = true;
-            recommendedProxySettings = true;
-          };
-        };
-      };
+      inherit nixosModules;
     };
 }
+
+# Convenience function to setup a simple reverse proxy
+#   setupReverseProxy = { domain, proxy_socket }: {
+#     config.services.nginx.virtualHosts.${domain} = {
+#       forceSSL = true;
+#       acmeRoot = null;
+#       enableACME = true;
+
+#       locations."/" = {
+#         proxyPass = proxy_socket;
+#         proxyWebsockets = true;
+#         recommendedProxySettings = true;
+#       };
+#     };
+#   };
